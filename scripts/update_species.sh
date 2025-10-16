c14e0f89a76d28c752da26352fbbcf2489be5ad0
Daniela Cuartas <42179443+dani-cuar@users.noreply.github.com>
2025-10-15 20:47:12 -0700

    Update update_species.sh

--- scripts/update_species.sh
+++ scripts/update_species.sh
@@ -2,5 +2,5 @@
 # Update the species list
 #set -x
 source /etc/birdnet/birdnet.conf
-sqlite3 $HOME/BirdNET-Pi/scripts/birds.db "SELECT DISTINCT(Com_Name) FROM detections" | sort >  ${IDFILE}
+#sqlite3 $HOME/BirdNET-Pi/scripts/birds.db "SELECT DISTINCT(Com_Name) FROM detections" | sort >  ${IDFILE}
 sqlite3 $HOME/BirdNET-Pi/scripts/detections_whale.db "SELECT DISTINCT(Com_Name) FROM detections" | sort >  ${IDFILE}
